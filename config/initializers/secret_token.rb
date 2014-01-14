2417c48a4ec3e3d88ea33405656f3ec54ffa97b2
Valeria <atargatis.the.one@gmail.com>
2014-01-14 12:38:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marketplace::Application.config.secret_key_base = '86ba140b3b88ca511093416502629e559fbd9635f5ba4ac8105e25bbdc42db7844e60c20a6e21e6087619ea1538a1b75846afddf681fae5f3408f836dfd53c81'
