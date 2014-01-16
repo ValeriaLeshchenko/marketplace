15ee140bf3aed814fc47898b51881a4a3c208109
Valeria <atargatis.the.one@gmail.com>
2014-01-16 17:55:02 +0200

    Edit seeds and layout

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ end
 group :development, :test do
   gem 'rspec-rails', '~> 3.0.0.beta'
   gem "factory_girl_rails"
+  gem 'railroady'
 end
 
 gem 'devise'
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,33 @@ sizes = Size.create([{ name: 'XS' },
                      { name: 'XXL' },
                      { name: 'XXXL' }
 ])
+
+
+
+products = Product.create([{name: 'Trousers', description: 'Checked men\'s trousers', company: 'Zara', count: 10,
+                picture: File.open(File.join(Rails.root, 'public/trousers.jpeg'))},
+                {name: 'Chiffon dance skirt', description: 'Very nice handmade four panel chiffon skirt
+                with sequins', company: 'Atlanta belly dance', count: 4,
+                picture: File.open(File.join(Rails.root, 'public/skirt.jpg'))},
+                {name: 'Trainers', description: 'Mizuno wave stylish trainers', company: 'Mizuno', count: 15,
+                picture: File.open(File.join(Rails.root, 'public/shoes.jpg'))}
+])
+
+prices = Price.create([{ amount: 5.23, currency: 'USD', product: products[0]},
+                       { amount: 14.30, currency: 'RUB', product: products[1]},
+                       { amount: 3.40, currency: 'EUR', product: products[2]}
+])
+
+products[0].colors << colors[0]
+products[0].colors << colors[1]
+products[1].colors << colors[3]
+products[2].colors << colors[5]
+products[2].colors << colors[7]
+products[2].colors << colors[9]
+
+
+products[0].sizes << sizes[2]
+products[0].sizes << sizes[3]
+products[0].sizes << sizes[4]
+products[1].sizes << sizes[2]
+products[2].sizes << sizes[5]
